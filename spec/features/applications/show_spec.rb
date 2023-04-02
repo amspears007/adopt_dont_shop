5e38cdf8e7e6dfd0b6d43b0012cf8cd2378ab9ad
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-01 18:53:43 -0600

    Test: update tests for appliction in user story 6

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -72,22 +72,11 @@ RSpec.describe 'Application Show', type: :feature do
 
       fill_in("Description", with: "I love cats so I want to adopt them a lot")
       click_on("Submit Application")
-save_and_open_page
+
       expect(page).to have_content("Pending")
       expect(page).to have_no_content("In Progress")
       expect(page).to have_content("I love cats so I want to adopt them a lot")
       expect(page).to have_no_content("Add a Pet to this Application")
     end
   end
-#   As a visitor
-# When I visit an application's show page
-# And I have added one or more pets to the application
-# Then I see a section to submit my application
-# And in that section I see an input to enter why I would make a good owner for these pet(s)
-# When I fill in that input
-# And I click a button to submit this application
-# Then I am taken back to the application's show page
-# And I see an indicator that the application is "Pending"
-# And I see all the pets that I want to adopt
-# And I do not see a section to add more pets to this application
 end
