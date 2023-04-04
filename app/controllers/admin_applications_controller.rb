ac81955dd50b60ebcdbe427425797e779264114c
Amy Spears <amspears007@gmail.com>
2023-04-03 18:53:01 -0600

    Update admin_app controller update method to adoption status and add if/else statmeent to html page

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,7 +1,14 @@
 class AdminApplicationsController < ApplicationController
   def show
-    require 'pry'; binding.pry
     @application = Application.find(params[:id])
     @pets = @application.pets
+  end
+
+  def update
+    require 'pry'; binding.pry
+    @application = Application.find(params[:id])
+    @pet = Pet.find(params[:pet_id])
+    @pet.update(adoption_status: params[:adoption_status])
+    redirect_to "/admin/applications/#{@application.id}"
   end
 end
